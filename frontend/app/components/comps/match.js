30b0baec01da35b2baf03e80efaae138034431b6
YusufLisawi <yusufisawi@gmail.com>
2024-04-16 11:46:08 +0100

    Tournament brackets generation done

--- frontend/app/components/comps/match.js
+++ frontend/app/components/comps/match.js
@@ -13,10 +13,13 @@ export default class Match extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === "match-id") {
             this.matchId = parseInt(newValue);
+            this.findMatch();
+            this.findPlayersNames();
             this.render();
         }
         if (name === "winner-id") {
             this.winnerId = parseInt(newValue);
+            this.findPlayersNames();
             this.render();
         }
     }
@@ -26,16 +29,12 @@ export default class Match extends HTMLElement {
     }
 
     connectedCallback() {
-        this.match = tournamentStore.getState().matches.find(match => match.id === this.matchId);
-        console.log(tournamentStore.getState().matches, this.match, this.matchId)
         this.render();
     }
 
     disconnectedCallback() {}
 
     render() {
-        this.findPlayersNames();
-
         this.innerHTML = /*html*/`
             <div class="player ${this.winnerId && this.winnerId === this.match.winner.id && 'winner'}">${this.player1Name}</div>
             <div class="player ${this.winnerId && this.winnerId === this.player2Id && 'winner'}">${this.player2Name}</div>
@@ -49,5 +48,10 @@ export default class Match extends HTMLElement {
         if (this.match) {
             this.player2Name = this.match.player2.alias ? this.match.player2.alias : 'Unknown Player';
         }
+    }
+
+    findMatch() {
+        this.match = tournamentStore.getState().matches.find(match => match.id === this.matchId);
+        console.log(this.match);
     }
 }
