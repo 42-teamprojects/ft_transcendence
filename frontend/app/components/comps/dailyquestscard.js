58986523c9e493348e45ef043f8f657210d4ad8a
YusufLisawi <yusufisawi@gmail.com>
2024-04-28 11:30:33 +0100

    Merging home

--- frontend/app/components/comps/dailyquestscard.js
+++ frontend/app/components/comps/dailyquestscard.js
@@ -12,9 +12,9 @@ export default class Dailyquestscard extends HTMLElement {
     render() {
         this.innerHTML = /*html*/`
         <div class="container card-border flex-col gap-4">
-            <div class="title-bar flex justify-between items-center">
+            <div class="title-bar flex justify-between items-center mb-3">
                 <h1>Daily quests</h1>
-                <a is="c-link" href="google.com" class="uppercase text-xs font-extrabold spacing-1">view all</a>
+                <a is="c-link" href="google.com" class="uppercase font-extrabold spacing-1">view all</a>
             </div>
             <div class="flex-col-center">
                 <c-taskcard></c-taskcard>
--- frontend/app/components/comps/friendscard.js
+++ frontend/app/components/comps/friendscard.js
@@ -12,9 +12,9 @@ export default class Friendscard extends HTMLElement {
     render() {
         this.innerHTML = /*html*/`
             <div class="container card-border flex-col gap-4">
-                <div class="title-bar flex justify-between items-center">
+                <div class="title-bar flex justify-between items-center mb-3">
                     <h1>Friends</h1>
-                    <a is="c-link" href="google.com" class="uppercase text-xs font-extrabold spacing-1">view all</a>
+                    <a is="c-link" href="google.com" class="uppercase font-extrabold spacing-1">view all</a>
                 </div>
                 <div class="grid grid-cols-4 grid-center gap-5">
                     <c-usercard username="Hassan" status="offline" img="https://api.dicebear.com/8.x/avataaars/svg?seed=Baby"></c-usercard>
