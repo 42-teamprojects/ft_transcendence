63547ebcc1e61d8bd7a556ce4011dc0b8e7f51b3
YusufLisawi <yusufisawi@gmail.com>
2024-04-30 11:12:19 +0100

    Some Login UI fixes

--- frontend/app/components/pages/login.js
+++ frontend/app/components/pages/login.js
@@ -11,14 +11,20 @@ export default class Login extends HTMLElement {
 
 	render() {
 		this.innerHTML = /*html*/ `
-        <div class="container">
+        <div class="form-container">
             <h1>Log in</h1>
-            <div>
-                <input type="text" class="input-field" placeholder="Username or email"/>
-                <input type="password" class="input-field" placeholder="Password" /> 
-                <a is="c-link" class="small" href="/password/forgot">FORGOT?</a>
+            <form>
+                <div class="form-group">
+                    <input type="text" class="input-field" placeholder="Username or email"/>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="input-field" placeholder="Password" />
+                    <span class="text-xs mr-1 text-right">
+                        <a is="c-link" href="/password/forgot">Forgot password?</a>
+                    </span>
+                </div>
                 <button is="c-button" class="btn-secondary ">Login</button>
-            </div>
+            </form>
             <p>Don't have an account? <a is="c-link" href="/register">Register here.</a></p>
             <div class="hr uppercase font-bold">
                 <hr> <span> or </span> <hr>
@@ -28,8 +34,6 @@ export default class Login extends HTMLElement {
                 <button is="c-button" class="btn-default"> <img src="/public/assets/icons/google.svg" alt="google"/> google </button>
             </div>
         </div>
-
-
         `;
 	}
 }
--- frontend/app/components/pages/sign-up.js
+++ frontend/app/components/pages/sign-up.js
@@ -1,4 +1,4 @@
-export default class Signup extends HTMLElement {
+export default class SignUp extends HTMLElement {
     constructor() {
         super();
     }
@@ -10,30 +10,38 @@ export default class Signup extends HTMLElement {
     disconnectedCallback() {}
 
     render() {
-        this.innerHTML = /*html*/`
-        <div class="container">
+        this.innerHTML = /*html*/ `
+        <div class="form-container">
             <h1>Sign up</h1>
-            <div>
-                <input type="text" class="input-field" placeholder="Full name"/>
-                <input type="text" class="input-field" placeholder="Username"/>
-                <input type="email" class="input-field" placeholder="Email"/>
-                <input type="password" class="input-field" placeholder="Password" /> 
-                <input type="password" class="input-field" placeholder="Confirm password" />
+            <form>
+                <div class="form-group">
+                    <input type="text" class="input-field" placeholder="Full name"/>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="input-field" placeholder="Username"/>
+                </div>
+                <div class="form-group">
+                    <input type="email" class="input-field" placeholder="Email"/>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="input-field" placeholder="Password" />
+                </div>
+                <div class="form-group">
+                    <input type="password" class="input-field" placeholder="Confirm password" />
+                </div>
                 <button is="c-button" class="btn-secondary ">Sign up</button>
-            </div>
+            </form>
             <p>Already signed up? <a is="c-link" href="/login">Log in here.</a></p>
-            <div class="hr uppercase font-bold">
-                <hr> <span> or </span> <hr>
             </div>
-            <div class="social-login">
-                <button is="c-button" class="btn-default"> <img src="/public/assets/icons/42.svg" alt="42"/> intra </button>
-                <button is="c-button" class="btn-default"> <img src="/public/assets/icons/google.svg" alt="google"/> google </button>
-            </div>
-
-        </div>
-
-        `;
+            `;
+            // <div class="hr uppercase font-bold">
+            //     <hr> <span> or </span> <hr>
+            // </div>
+            // <div class="social-login">
+            //     <button is="c-button" class="btn-default"> <img src="/public/assets/icons/42.svg" alt="42"/> intra </button>
+            //     <button is="c-button" class="btn-default"> <img src="/public/assets/icons/google.svg" alt="google"/> google </button>
+            // </div>
     }
 }
 
-customElements.define('p-signup', Signup);
+customElements.define("p-signup", SignUp);
