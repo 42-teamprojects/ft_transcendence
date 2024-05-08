ce0365a0fb49961edc8a56b8af65cf0d748057ed
YusufLisawi <yusufisawi@gmail.com>
2024-05-08 08:20:06 +0100

    Updates

--- frontend/app/components/comps/tournament-card.js
+++ frontend/app/components/comps/tournament-card.js
@@ -10,12 +10,13 @@ export default class Tournamentcard extends HTMLElement {
     disconnectedCallback() {}
 
     render() {
+        const type = 'notStarted'
         this.innerHTML = /*html*/`
         <div class="tournament-card">
             <div class="tournament-card-header">
                 <div class="tournament-card-header-text">
                     <div class="tournament-card-title">16-Players Tournament</div>
-                    <div class="tournament-card-subtitle">Waiting for players to join</div>
+                    <div class="tournament-card-subtitle">${this.getSubtitleContent(type)}</div>
                 </div>
                 <div class="tournament-card-players">
                     <a is="c-link" href="/user/1" tooltip="User" flow="up">
@@ -36,7 +37,7 @@ export default class Tournamentcard extends HTMLElement {
                 </div>
             </div>
             <div class="tournament-card-footer">
-                ${this.getFooterContent('join')}
+                ${this.getFooterContent(type)}
             </div>
         </div>
         `;
@@ -45,15 +46,15 @@ export default class Tournamentcard extends HTMLElement {
     getFooterContent(footer) {
         let footerContent;
         switch (footer) {
-            case 'join':
+            case 'waitingStart':
             footerContent = /*html*/`
-                <p class="tournament-card-footer-text">2 Left to join</p>
-                <button is="c-button" class="tournament-card-join-btn btn-primary">Join</button>
+                <p class="tournament-card-footer-text">All joined</p>
             `;
             break;
-            case 'waiting':
+            case 'waitingPlayers':
             footerContent = /*html*/`
-                <p class="tournament-card-footer-text">Waiting for organizer to start</p>
+            <p class="tournament-card-footer-text">3 Left to join</p>
+            <button is="c-button" class="tournament-card-join-btn btn-primary">Join</button>
             `;
             break;
             case 'start':
@@ -83,16 +84,26 @@ export default class Tournamentcard extends HTMLElement {
     getSubtitleContent(subtitle) {
         let subtitleContent;
         switch (subtitle) {
-            case 'waiting':
+            case 'waitingPlayers':
             subtitleContent = "Waiting for players to join";
+            break;
+            case 'waitingStart':
+                subtitleContent = "Waiting for organizer to start";
             break;
             case 'start':
             subtitleContent = "All players joined, starting soon";
             break;
-            case 'inProgress':
-            subtitleContent = "In progress";
+            case 'notStarted':
+            subtitleContent = "No organizer yet, be the first!";
             break;
-        }                
+            case 'inProgress':
+                subtitleContent = "In progress";
+            break;
+            default:
+            subtitleContent = '';
+            break;
+        }
+        return subtitleContent;       
     }
 
 }
