0424723246d7bc603d1da279f51991614da2c1f5
smouad <sodormouad@gmail.com>
2024-05-18 20:54:00 +0100

    models added for chat

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+class Chat(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
+
+class Message(models.Model):
+    content = models.TextField()
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    is_seen = models.BooleanField(default=False)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
