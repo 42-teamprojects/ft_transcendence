bd57974c699197dbd9ebb04b84c58a913f7b77fd
smouad <sodormouad@gmail.com>
2024-05-20 12:16:38 +0100

    first migrate chat

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -6,9 +6,15 @@ class Chat(models.Model):
     user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
     user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
 
+    def __str__(self):
+        return f'chat between {self.user1} and {self.user2}'
+
 class Message(models.Model):
     content = models.TextField()
     chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
     is_seen = models.BooleanField(default=False)
     sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'message from {self.sender_id} in {self.chat_id}'
