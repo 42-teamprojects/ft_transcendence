fe1ec87e38ef1730a60fdb30f870f5b290e75e0d
mouad sodor <sodormouad@gmail.com>
2024-05-20 22:13:06 +0100

    check for douplicated chats

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,10 +1,31 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .models import Chat, Message
 from .serializers import ChatSerializer, MessageSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from django.db.models import Q
 
+# class ChatListView(generics.ListCreateAPIView):
+#     queryset = Chat.objects.all()
+#     serializer_class = ChatSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
 class ChatListView(generics.ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        user1 = request.data.get('user1')
+        user2 = request.data.get('user2')
+
+        existing_chat = Chat.objects.filter(
+            Q(user1=user1, user2=user2) | Q(user1=user2, user2=user1)
+        ).first()
+
+        if existing_chat:
+            return Response(
+                {'detail': 'A chat between these users already exists.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return super().create(request, *args, **kwargs)
