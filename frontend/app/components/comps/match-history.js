c8c057541d0113301e84c99ff8d5e1fadb4f01b3
YusufLisawi <yusufisawi@gmail.com>
2024-05-23 08:38:00 +0100

    Updates

--- frontend/app/components/comps/match-history.js
+++ frontend/app/components/comps/match-history.js
@@ -7,6 +7,28 @@ export default class Matchhistory extends HTMLElement {
         this.render();
     }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'isWinner') {
+            this.isWinner = newValue;
+        }
+        if (name === 'player1') {
+            this.player1 = newValue;
+        }
+        if (name === 'player2') {
+            this.player2 = newValue;
+        }
+        if (name === 'score1') {
+            this.score1 = newValue;
+        }
+        if (name === 'score2') {
+            this.score2 = newValue;
+        }
+    }
+
+    static get observedAttributes() {
+        return ['isWinner', 'player1', 'player2', 'score1', 'score2'];
+    }
+
     disconnectedCallback() {}
 
     render() {
@@ -19,14 +41,15 @@ export default class Matchhistory extends HTMLElement {
                     <h4 class="text-success">5</h4>
                 </div>
             </div>
-            <div class="match-history__state">
-                <i class="fa fa-trophy text-success"></i>
+            <div class="match-history__state isLoser">
+                <i class="fa fa-times text-danger"></i>
+                <i class="fa fa-check text-success"></i>
             </div>
             <div class="match-history__player">
                 <img src="https://api.dicebear.com/8.x/thumbs/svg?seed=mouad" alt="" />
                 <div class="match-history__player-details">
                     <h3>Jane</h3>
-                    <h4 class="loser text-danger">3</h4>
+                    <h4 class="text-danger">3</h4>
                 </div>
             </div>
         </div>
