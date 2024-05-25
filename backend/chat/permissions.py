db4d6c060db068539efccc65770e664c59435bfa
smouad <sodormouad@gmail.com>
2024-05-25 15:16:04 +0100

    user can only see chats that involves him

--- backend/chat/permissions.py
+++ backend/chat/permissions.py
@@ -3,8 +3,9 @@ from .models import Chat, Message
 
 class IsChatParticipant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        user = request.user
         if isinstance(obj, Chat):
-            return obj.user1 == request.user or obj.user2 == request.user
+            return obj.user1 == user or obj.user2 == user
         if isinstance(obj, Message):
-            return obj.chat.user1 == request.user or obj.chat.user2 == request.user
+            return obj.chat.user1 == user or obj.chat.user2 == user
     
