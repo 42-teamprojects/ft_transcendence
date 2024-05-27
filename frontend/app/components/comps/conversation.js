d7632c791bc02c7422f05129d8f914fc70e28f30
YusufLisawi <yusufisawi@gmail.com>
2024-05-27 20:29:08 +0100

    Updated chat

--- frontend/app/components/comps/conversation.js
+++ frontend/app/components/comps/conversation.js
@@ -4,13 +4,17 @@ import { getMatchUrl } from "../../utils/utils.js";
 export default class Conversation extends HTMLElement {
     constructor() {
         super();
-        this.chatId = getMatchUrl(/^\/dashboard\/chat\/(\w+)$/) || "none";
-        if (this.chatId === "none") {
-            throw new Error("Chat id not found");
+        this.isEmpty = window.location.href.match(/\/chat\/?$/);
+        
+        if (!this.isEmpty) {
+            this.chatId = getMatchUrl(/^\/dashboard\/chat\/(\w+)$/) || "none";
+            if (this.chatId === "none") {
+                throw new Error("Chat id not found");
+            }
+            this.chat = chatService.getState().chats.find((chat) => {
+                return chat.id === parseInt(this.chatId);
+            });;
         }
-        this.chat = chatService.getState().chats.find((chat) => {
-            return chat.id === parseInt(this.chatId);
-        });;
     }
 
     connectedCallback() {
@@ -22,9 +26,22 @@ export default class Conversation extends HTMLElement {
     render() {
         this.innerHTML = /*html*/`
         <div class="conversation vh-full w-full">
+            ${this.isEmpty
+            ? /*html*/`
+                <div class="flex-center vh-full">
+                <div class="flex-col-center gap-4">
+                    <i class="fa-regular fa-comments text-6xl text-primary mb-5"></i>
+                    <h1 class="text-xl font-medium">Select a conversation to start chatting</h1>
+                    <h2 class="text-md font-normal text-stroke">Or find a friend and chat</h2>
+                    <button class="btn-primary" onclick="document.querySelector('c-chat-search-modal').open()">Find friends</button>
+                </div>
+                </div>
+            `     
+            : /*html*/`
             <c-conversation-header img="https://api.dicebear.com/8.x/thumbs/svg?seed=mouad" username="${this.chat.friend.username}" state="${this.chat.friend.status}"></c-conversation-header>
             <c-conversation-body></c-conversation-body>
             <c-conversation-footer></c-conversation-footer>
+            `}
         </div>
         `;
     }
--- frontend/app/components/pages/chat.js
+++ frontend/app/components/pages/chat.js
@@ -1,34 +1,28 @@
-import ChatApiService from "../../api/chat/chatApiService.js";
-import Router from "../../router/router.js";
-import { chatService } from "../../state/chatService.js";
-
 export default class Chat extends HTMLElement {
-    constructor() {
-        super();
-        document.title = 'Chat | Blitzpong';
-        this.isEmpty = window.location.href.match(/\/chat\/?$/);
-    }
+	constructor() {
+		super();
+		document.title = "Chat | Blitzpong";
+		this.isEmpty = window.location.href.match(/\/chat\/?$/);
+	}
 
-    async connectedCallback() {
-        try {
-            await chatService.getChats();
-            this.render();
-        } catch (error) {
-            Router.instance.back();
-        }
-    }
+	async connectedCallback() {
+		this.render();
+	}
 
-    disconnectedCallback() {}
+	disconnectedCallback() {}
 
-    render() {
-        this.innerHTML = /*html*/`
-        <div class=${this.isEmpty ? 'chat-page__empty' : 'chat-page'}>
+	render() {
+		this.innerHTML = /*html*/ `
+
+        <div class='chat-page'>
             <c-chat-list></c-chat-list>
-            <c-conversation username="msodor" img="https://api.dicebear.com/8.x/thumbs/svg?seed=mouad"></c-conversation>
-            <c-chat-match-history></c-chat-match-history>
+            <c-conversation></c-conversation>
+            ${!this.isEmpty ? 
+            /*html*/ `<c-chat-match-history></c-chat-match-history>` 
+            : ""}
         </div>
         `;
-    }
+	}
 }
 
-customElements.define('p-chat', Chat);
+customElements.define("p-chat", Chat);
