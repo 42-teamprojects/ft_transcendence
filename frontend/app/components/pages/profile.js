752233415524e22b2c6a70e59e1a47356193f776
hamzatlh <htlhaouia@gmail.com>
2024-05-29 17:04:31 +0100

    updates front

--- frontend/app/components/pages/profile.js
+++ frontend/app/components/pages/profile.js
@@ -14,15 +14,22 @@ export default class Profile extends HTMLElement {
         <div class="dashboard-content">
             <main>
                 <section class="profile-info">
-                    <div class="info">
-                        <h2>Hamza Talhaouia</h2>
-                        <p>htalhaou</p>
-                        <h3>Joined February 2023</h3>
-                    </div>
                     <div class="profile-image">
                         <img src="https://api.dicebear.com/8.x/thumbs/svg?seed=hamza" alt="profile image">
                     </div>
-                    <hr><span></span>
+                    <div class="profile-user">
+                        <div class="profile-user-names">
+                            <h2>Hamza Talhaouia</h2>
+                            <p>htalhaou</p>
+                        </div>
+                        <h3>Joined February 2023</h3>
+                    </div>
+                </section>
+                <hr class="divider">
+                <section class="profile-bio">
+                <div class="settings-header mb-9">
+                        <h2 class="mb-3">Statistics</h2>
+                    </div>
                 </section>
             </main>
             <div class="widgets flex-col-center gap-5">
