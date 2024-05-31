9610d8bebdebd96e170c7c88ee0113f174c2416e
smouad <sodormouad@gmail.com>
2024-05-31 17:16:41 +0100

    adding custum notification permossion

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Notification
 from .serializers import NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+    queryset = Notification.objects.all()
+    permission_classes = [IsAuthenticated, IsNotificationRecipient]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notification.objects.filter(recipient=user)
