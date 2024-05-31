6db7ca08c28713017bace1f0d6f9192a798562a4
YusufLisawi <yusufisawi@gmail.com>
2024-05-31 11:39:30 +0100

    Added upload modal

--- frontend/app/components/pages/profile.js
+++ frontend/app/components/pages/profile.js
@@ -16,12 +16,13 @@ export default class Profile extends HTMLElement {
 
     render() {
         this.innerHTML = /*html*/`
+        <c-upload-avatar-modal></c-upload-avatar-modal>
         <div class="dashboard-content">
             <main>
                 <section class="profile-info">
                     <div class="profile-image relative">
                         <img src="https://api.dicebear.com/8.x/thumbs/svg?seed=${this.user.username}" alt="profile image">
-                        <div class="absolute bg-secondary p-2 rounded-full border-white cursor-pointer w-8 h-8 flex-center" style="top: 10px; right: 10px">
+                        <div class="absolute bg-secondary p-2 rounded-full border-white cursor-pointer w-8 h-8 flex-center" style="top: 10px; right: 10px" onclick="document.querySelector('c-upload-avatar-modal').open()">
                             <i class="fa-solid fa-camera"></i>
                         </div>
                     </div>
