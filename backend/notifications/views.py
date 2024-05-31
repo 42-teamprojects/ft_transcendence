906b01061ed3208e2eb25226a11525b8f46941d2
smouad <sodormouad@gmail.com>
2024-05-31 16:37:16 +0100

    starting serializers and views

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Notification
+from .serializers import NotificationSerializer
 # Create your views here.
 
 
