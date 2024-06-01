00f161962d4c274760d5adbd69f3087807f9c1e3
smouad <sodormouad@gmail.com>
2024-06-01 15:36:59 +0100

    Add notification creation and retrieval methods to NotificationState

--- frontend/app/state/notificationState.js
+++ frontend/app/state/notificationState.js
@@ -1,7 +1,44 @@
+import HttpClient from "../http/httpClient";
 import State from "./state"
 
 class NotificationState extends State {
     constructor() {
-        super({})
+        super({
+            notification: {},
+            loading: true,
+        })
+        this.httpClient = HttpClient.instance;
+    }
+
+    async createNotification(notification) {
+        try {
+            this.resetLoading();
+            await this.httpClient.post('notifications/', notification);
+            this.getNotifications();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getNotifications() {
+        try {
+            this.resetLoading();
+            const notifications = await this.httpClient.get('notifications/');
+            this.setState({ notifications, loading: false });
+        } catch (error) {
+            this.setState({ notifications: [], loading: false });
+            console.error(error);
+        }
+    }
+
+    resetLoading() {
+        this.setState({ loading: true });
+    }
+
+    reset() {
+        this.setState({
+            notifications: [],
+            loading: true,
+        });
     }
 }
