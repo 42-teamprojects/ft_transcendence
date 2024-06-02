bd68f342d9d182d5d454ccd6f6b2be33f0741497
HassanOigag <hassanoigag0@gmail.com>
2024-06-02 23:54:42 +0100

    added three dot animation

--- frontend/app/components/pages/match-making.js
+++ frontend/app/components/pages/match-making.js
@@ -3,11 +3,38 @@ import Router from "../../router/router.js";
 import { userState } from "../../state/userState.js";
 import { matchState } from "../../state/matchState.js";
 
+
+class ThreeDots extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    disconnectedCallback() {}
+
+    render() {
+        this.innerHTML = /*html*/`
+        <div class="flex align-end gap-5">
+            <h1>Searching</h1>
+            <div class="dots">
+                <div class="single-dot"></div>
+                <div class="single-dot"></div>
+                <div class="single-dot"></div>
+            </div>    
+        </div>
+        `;
+    }
+}
+
+customElements. define('c-three-dots', ThreeDots);
 export default class Matchmaking extends HTMLElement {
     constructor() {
         super();
         this.user = userState.state.user;
-        this.opponent = {username: "Searching...", avatar: "/storage/avatars/default.png"};
+        this.opponent = {username: "Searching...", avatar: "/storage/avatars/default.jpg"};
     }
     
     connectedCallback() {
@@ -52,18 +79,18 @@ export default class Matchmaking extends HTMLElement {
     render() {
         this.innerHTML = /*html*/`
             <div class="match-making-container flex-col  flex-center gap-40">
-                <h1 class="starting">${(this.opponent.username == "Searching...") ? "Searching..." : "Starting in 3..."}</h1>
+                <h1 class="starting">${(this.opponent.username == "Searching...") ? /*html*/`<c-three-dots></c-three-dots>`: "Starting in 3..."}</h1>
                 <div class="flex flex-center gap-30">
                     <div class="flex flex-col gap-5">
-                        <div class="img-box">
+                        <div class="img-box player-border">
                             <img src="${config.backend_domain}${this.user.avatar}" alt="player1" class="player-img">
                         </div>
                         <h2 class="text-secondary text-3xl">${this.user.username}</h2>
                     </div>
                     <h1 class="text-6xl font-black spacing-10">VS</h1>
                     <div class="flex flex-col gap-5">
-                        <div class="img-box">
-                            <img src="${config.backend_domain}${this.opponent.avatar}" alt="player1" class="player-img">
+                        <div class="img-box loading">
+                            <img src="${config.backend_domain}${this.opponent.avatar}" alt="" class="player-img">
                         </div>
                         <h2 class="text-primary text-3xl">${this.opponent.username}</h2>
                     </div>
