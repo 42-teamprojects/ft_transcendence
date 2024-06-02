6706a1eb8e4c179a1e1f161b00323234906a940d
HassanOigag <hassanoigag0@gmail.com>
2024-06-02 14:23:05 +0100

    Refactor player images in match-making page

--- frontend/app/components/pages/match-making.js
+++ frontend/app/components/pages/match-making.js
@@ -27,13 +27,17 @@ export default class Matchmaking extends HTMLElement {
                 <h1>Match starts in ...3</h1>
                 <div class="flex flex-center gap-30">
                     <div class="flex flex-col gap-5">
-                        <img src="${config.backend_domain}${this.user.avatar}" alt="player1" class="player-img">
+                        <div class="img-box">
+                            <img src="${config.backend_domain}${this.user.avatar}" alt="player1" class="player-img">
+                        </div>
                         <h2 class="text-secondary text-3xl">${this.user.username}</h2>
                     </div>
                     <h1 class="text-6xl font-black spacing-10">VS</h1>
                     <div class="flex flex-col gap-5">
-                        <img src="https://plus.unsplash.com/premium_photo-1716138192476-f34e85ad43c2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" alt="player1" class="player-img">
-                        <h2 class="text-primary text-3xl">Player 2</h2>
+                        <div class="img-box">
+                            <img src="https://plus.unsplash.com/premium_photo-1716138192476-f34e85ad43c2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" alt="player1" class="player-img">
+                        </div>
+                        <h2 class="text-primary text-3xl">Searching...</h2>
                     </div>
                 </div>
                 <button class="btn-primary">quit match</button>
