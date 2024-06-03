8416ce9e9280d13fca0361f6f11b7159415ec16a
YusufLisawi <yusufisawi@gmail.com>
2024-06-03 22:55:24 +0100

    Updates

--- backend/tournaments/models.py
+++ backend/tournaments/models.py
@@ -9,6 +9,8 @@ import math
 import random
 from django.utils import timezone
 
+import tournaments
+
 class Tournament(models.Model):
     TYPE_CHOICES = [
         ('4', '4-players'),
@@ -34,6 +36,8 @@ class Tournament(models.Model):
                 raise ValidationError('There\'s already an active tournament of this type.')
             if Tournament.objects.filter(organizer=self.organizer, status='NS').exists():
                 raise ValidationError('You are already organizing an active tournament.')
+            if self.type not in [choice[0] for choice in self.TYPE_CHOICES]:
+                raise ValidationError('Invalid tournament type.')
             super().save(*args, **kwargs)
             self.participants.add(self.organizer)
         else:
@@ -47,14 +51,17 @@ class Tournament(models.Model):
 
         participants = self.randomize_participants()
         matches = self.generate_matches(participants)
+        print(matches)
         TournamentMatch.objects.bulk_create(matches)
         # self.notify_participants()
 
     def validate_start_conditions(self):
         if self.status != 'NS':
             raise ValidationError('The tournament has already started.')
-        # if self.participants.count() < int(self.type):
-        #     raise ValidationError('Not enough participants to start the tournament.')
+        if self.participants.count() > int(self.type):
+            raise ValidationError('The tournament is already full.')
+        if self.participants.count() < int(self.type):
+            raise ValidationError('The tournament is not full yet.')
 
     def calculate_rounds(self):
         num_rounds = int(math.log2(self.participants.count()))
@@ -67,23 +74,36 @@ class Tournament(models.Model):
 
     def generate_matches(self, participants):
         matches = []
+        match_number = 0  # Initialize match_number
         for round in range(1, self.total_rounds + 1):
             group = 1
-            num_matches = int(self.participants.count() / 2**(round - 1))
+            num_matches = int(2**(self.total_rounds - round))
             for i in range(num_matches):
                 player1 = participants.pop() if round == 1 and participants else None
                 player2 = participants.pop() if round == 1 and participants else None
                 if player1 and player2:
-                    matches.append(TournamentMatch(tournament=self, round=round, group=group, player1=player1, player2=player2))
-                elif player1 and not player2:
-                    matches.append(TournamentMatch(tournament=self, round=round, group=group, player1=player1, player2=None, status='F', winner=player1))
-                if i % 2 == 1: # Increment the group number every two matches
+                    matches.append(TournamentMatch(tournament=self, round=round, group=group, match_number=match_number, player1=player1, player2=player2))
+                else:
+                    matches.append(TournamentMatch(tournament=self, round=round, group=group, match_number=match_number, player1=None, player2=None))
+                if i % 2 == 1:  # Increment the group number and reset match_number every two matches
                     group += 1
+                    match_number = 0
+                else:  # Increment match_number for every match
+                    match_number += 1
         return matches
+
+    def add_participant(self, user):
+        if self.status != 'NS':
+            raise ValidationError('Cannot add participants to a tournament that has already started.')
+        if self.participants.count() >= int(self.type):
+            raise ValidationError('The tournament is already full.')
+        self.participants.add(user)
+        self.save()
     
-    def print_matches_tree(self):
-        matches = self.matches.all()
-        for match in matches:
+    # print tournament qualification brackets tree
+    def print_tree(self):
+        print(f'Tournament {self.pk} - {self.type} players')
+        for match in self.matches.all():
             print(f'Round {match.round} - Group {match.group}: {match.player1} vs {match.player2}')
 
     def notify_participants(self):
@@ -106,6 +126,7 @@ class TournamentMatch(models.Model):
         ('IP', 'In Progress'),
         ('F', 'Finished'),
     ]
+    match_number = models.IntegerField(null=True)
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='matches', null=True)
     round = models.IntegerField()
     group = models.IntegerField()
@@ -114,6 +135,14 @@ class TournamentMatch(models.Model):
     winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='NS')
     start_time = models.DateTimeField(auto_now_add=False, null=True)
+    
+    # on create, if the match is in the first round and doesn't have two players, set it as finished with the winner as player1, and move him to the next round
+    def save(self, *args, **kwargs):
+        if self.pk is None and self.round == 1 and self.player1 and not self.player2:
+            self.status = 'F'
+            self.winner = self.player1
+            self.assign_winner_to_next_round(self.player1)
+        super().save(*args, **kwargs)
     
     def set_winner(self, winner):
         if winner not in [self.player1, self.player2]:
@@ -124,14 +153,19 @@ class TournamentMatch(models.Model):
         self.save()
 
     def assign_winner_to_next_round(self, winner):
-        # Get the match in the next round for this match's group
-        next_round_match = TournamentMatch.objects.select_for_update().filter(tournament=self.tournament, round=self.round + 1, group=self.group).first()
+        # Calculate the group and match for the next round
+        next_round_group = self.group // 2
+        next_round_match_number = self.group % 2
+
+        # Get the match in the next round for the calculated group and match
+        next_round_match = TournamentMatch.objects.select_for_update().filter(
+            tournament=self.tournament, round=self.round + 1, group=next_round_group, match_number=next_round_match_number).first()
 
         if next_round_match:
-            # Assign the winner to player1 or player2, depending on which is empty
-            if next_round_match.player1 is None:
+            # Assign the winner to player1 or player2, depending on the current match number
+            if self.match_number == 0:
                 next_round_match.player1 = winner
-            elif next_round_match.player2 is None:
+            elif self.match_number == 1:
                 next_round_match.player2 = winner
             next_round_match.save()
 
@@ -139,3 +173,6 @@ class TournamentMatch(models.Model):
             if next_round_match.player1 is not None and next_round_match.player2 is not None:
                 next_round_match.start_time = timezone.now() + timedelta(minutes=1)
                 next_round_match.save()
+    
+    def __str__(self):
+        return f'Round {self.round} - {"1st match of" if self.match_number == 0 else "2nd match of"} Group {self.group}: {self.player1} vs {self.player2}, Winner: {self.winner}'
--- backend/tournaments/tests.py
+++ backend/tournaments/tests.py
@@ -1,36 +1,189 @@
-from django.forms import ValidationError
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from accounts.models import User
-from .models import Tournament
+import match
+from .models import Tournament, TournamentMatch
 
 class TournamentModelTest(TestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username='user1', email="user1@email.com", full_name="userr", password='pass')
-        self.user2 = User.objects.create_user(username='user2', email="user2@email.com", full_name="userr", password='pass')
-        self.user3 = User.objects.create_user(username='user3', email="user3@email.com", full_name="userr", password='pass')
-        self.user4 = User.objects.create_user(username='user4', email="user4@email.com", full_name="userr", password='pass')
+        usernames = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10', 'user11', 'user12', 'user13', 'user14', 'user15', 'user16']
+        emails = ['user1@example.com', 'user2@example.com', 'user3@example.com', 'user4@example.com', 'user5@example.com', 'user6@example.com', 'user7@example.com', 'user8@example.com', 'user9@example.com', 'user10@example.com', 'user11@example.com', 'user12@example.com', 'user13@example.com', 'user14@example.com', 'user15@example.com', 'user16@example.com']
+        self.users = [User.objects.create(username=username, email=email) for username, email in zip(usernames, emails)]
 
-    def test_create_tournament(self):
-        tournament = Tournament.objects.create(type='4', organizer=self.user1)
-        self.assertEqual(tournament.type, '4')
-        self.assertEqual(tournament.organizer, self.user1)
-        self.assertEqual(tournament.status, 'NS')
+    # def test_create_tournament(self):
+    #     print("Test: create_tournament")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     self.assertEqual(tournament.status, 'NS')
+    #     self.assertEqual(tournament.total_rounds, None)
+    #     self.assertIn(self.users[0], tournament.participants.all())
 
-    def test_start_tournament(self):
-        tournament = Tournament.objects.create(type='4', organizer=self.user1)
-        tournament.participants.add(self.user2, self.user3, self.user4)
-        tournament.start()
-        self.assertEqual(tournament.status, 'IP')
+    # def test_create_tournament_with_same_type(self):
+    #     print("Test: create_tournament_with_same_type")
+    #     Tournament.objects.create(type='4', organizer=self.users[0])
+    #     with self.assertRaises(ValidationError):
+    #         Tournament.objects.create(type='4', organizer=self.users[1])
 
-    # def test_start_tournament_not_enough_participants(self):
-    #     tournament = Tournament.objects.create(type='4', organizer=self.user1)
-    #     tournament.participants.add(self.user2)
+    # def test_create_tournament_with_same_organizer(self):
+    #     Tournament.objects.create(type='4', organizer=self.users[0])
+    #     print("Test: create_tournament_with_same_organizer")
+    #     with self.assertRaises(ValidationError):
+    #         Tournament.objects.create(type='8', organizer=self.users[0])
+
+    # def test_create_tournament_with_invalid_type(self):
+    #     print("Test: create_tournament_with_invalid_type")
+    #     with self.assertRaises(ValidationError):
+    #         Tournament.objects.create(type='5', organizer=self.users[0])
+
+    # def test_start_tournament(self):
+    #     print("Test: start_tournament")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3])
+    #     tournament.start()
+    #     self.assertEqual(tournament.status, 'IP')
+    #     self.assertEqual(tournament.total_rounds, 2)
+
+    # def test_tournament_4_rounds(self):
+    #     print("Test: tournament_4_rounds")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3])
+    #     tournament.start()
+    #     # tournament.print_tree()
+    #     self.assertEqual(tournament.status, 'IP')
+
+    # def test_tournament_8_rounds(self):
+    #     print("Test: tournament_8_rounds")
+    #     tournament = Tournament.objects.create(type='8', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3], self.users[4], self.users[5], self.users[6], self.users[7])
+    #     tournament.start()
+    #     # tournament.print_tree()
+    #     self.assertEqual(tournament.status, 'IP')
+    #     self.assertEqual(tournament.total_rounds, 3)
+
+    # def test_tournament_16_rounds(self):
+    #     print("Test: tournament_16_rounds")
+    #     tournament = Tournament.objects.create(type='16', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3], self.users[4], self.users[5], self.users[6], self.users[7], self.users[8], self.users[9], self.users[10], self.users[11], self.users[12], self.users[13], self.users[14], self.users[15])
+    #     tournament.start()
+    #     # tournament.print_tree()
+    #     self.assertEqual(tournament.status, 'IP')
+    #     self.assertEqual(tournament.total_rounds, 4)
+
+    # def test_create_tournament_with_less_than_required(self):
+    #     print("Test: create_tournament_with_less_than_required")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1])
     #     with self.assertRaises(ValidationError):
     #         tournament.start()
 
-    def test_start_tournament_already_started(self):
-        tournament = Tournament.objects.create(type='4', organizer=self.user1)
-        tournament.participants.add(self.user2, self.user3, self.user4)
-        tournament.start()
-        with self.assertRaises(ValidationError):
-            tournament.start()
+    # def test_create_tournament_with_more_than_required(self):
+    #     print("Test: create_tournament_with_more_than_required")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3], self.users[4])
+    #     with self.assertRaises(ValidationError):
+    #         tournament.start()
+
+    # def test_add_participant(self):
+    #     print("Test: add_participant")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.add_participant(self.users[1])
+    #     self.assertIn(self.users[1], tournament.participants.all())
+
+    # def test_add_participant_to_started_tournament(self):
+    #     print("Test: add_participant_to_started_tournament")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3])
+    #     tournament.start()
+    #     with self.assertRaises(ValidationError):
+    #         tournament.add_participant(self.users[4])
+
+    # def test_add_participant_to_full_tournament(self):
+    #     print("Test: add_participant_to_full_tournament")
+    #     tournament = Tournament.objects.create(type='4', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3], self.users[4])
+    #     with self.assertRaises(ValidationError):
+    #         tournament.add_participant(self.users[5])
+
+    # def test_print_tree(self):
+    #     print("Test: print_tree")
+    #     tournament = Tournament.objects.create(type='8', organizer=self.users[0])
+    #     tournament.participants.add(self.users[1], self.users[2], self.users[3], self.users[4], self.users[5], self.users[6], self.users[7])
+    #     tournament.start()
+    #     tournament.print_tree()
+
+class TournamentMatchModelTest(TestCase):
+    def setUp(self):
+        print("Settings up TournamentMatchModelTest")
+        usernames = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10', 'user11', 'user12', 'user13', 'user14', 'user15', 'user16']
+        emails = ['user1@example.com', 'user2@example.com', 'user3@example.com', 'user4@example.com', 'user5@example.com', 'user6@example.com', 'user7@example.com', 'user8@example.com', 'user9@example.com', 'user10@example.com', 'user11@example.com', 'user12@example.com', 'user13@example.com', 'user14@example.com', 'user15@example.com', 'user16@example.com']
+        self.users = [User.objects.create(username=username, email=email) for username, email in zip(usernames, emails)]
+        self.tournament_4 = Tournament.objects.create(type='4', organizer=self.users[0])
+        self.tournament_4.participants.add(self.users[1], self.users[2], self.users[3])
+        self.tournament_4.start()
+        # self.tournament_8 = Tournament.objects.create(type='8', organizer=self.users[0])
+        # self.tournament_8.participants.add(self.users[1], self.users[2], self.users[3], self.users[4], self.users[5], self.users[6], self.users[7])
+        # self.tournament_8.start()
+
+    # def test_create_match(self):
+    #     print("Test: create_match")
+    #     match = TournamentMatch.objects.create(tournament=self.tournament_4, round=1, group=1, player1=self.users[0], player2=self.users[1])
+    #     self.assertEqual(match.status, 'NS')
+    #     self.assertEqual(match.winner, None)
+
+    # def test_set_winner(self):
+    #     print("Test: set_winner")
+    #     match = TournamentMatch.objects.create(tournament=self.tournament_4, round=1, group=1, player1=self.users[0], player2=self.users[1])
+    #     match.set_winner(self.users[0])
+    #     self.assertEqual(match.winner, self.users[0])
+    #     self.assertEqual(match.status, 'F')
+    
+    def test_assign_winner_to_next_round_same_group(self):
+        print("Test: assign_winner_to_next_round_same_group")
+        match1 = TournamentMatch.objects.get(tournament=self.tournament_4, round=1, group=1, match_number=0)
+        match2 = TournamentMatch.objects.get(tournament=self.tournament_4, round=1, group=1, match_number=1)
+        match1.set_winner(match1.player1)
+        match2.set_winner(match2.player2)
+        next_round_match = TournamentMatch.objects.filter(tournament=self.tournament_4)
+        print(next_round_match)
+        # self.assertEqual(next_round_match.player1, self.users[1])
+        # self.assertEqual(next_round_match.player2, self.users[3])
+
+    # def test_assign_winner_to_next_round_different_group(self):
+    #     print("Test: assign_winner_to_next_round_different_group")
+    #     match1 = TournamentMatch.objects.create(tournament=self.tournament_8, round=1, group=1, player1=self.users[0], player2=self.users[1])
+    #     match2 = TournamentMatch.objects.create(tournament=self.tournament_8, round=1, group=2, player1=self.users[2], player2=self.users[3])
+    #     match1.set_winner(self.users[1])
+    #     match2.set_winner(self.users[3])
+    #     next_round_matches = TournamentMatch.objects.filter(tournament=self.tournament_8, round=2, group=1)
+    #     print(next_round_matches)
+        # get the match with smallest id
+        # next_round_match1 = next_round_matches.order_by('id').first()
+        # next_round_match2 = next_round_matches.order_by('id').last()
+        # self.assertEqual(next_round_match1.player1, self.users[1])
+        # self.assertEqual(next_round_match2.player1, self.users[3])        
+
+    
+#     def test_set_invalid_winner(self):
+#         match = TournamentMatch.objects.create(tournament=self.tournament_4, round=1, group=1, player1=self.users[0], player2=self.users[1])
+#         with self.assertRaises(ValidationError):
+#             match.set_winner(self.users[0])
+
+#     def test_assign_winner_to_next_round(self):
+#         match1 = TournamentMatch.objects.create(tournament=self.tournament_4, round=1, group=1, player1=self.users[0], player2=self.users[1])
+#         match2 = TournamentMatch.objects.create(tournament=self.tournament_4, round=1, group=2, player1=self.users[2], player2=self.users[4])
+#         match1.set_winner(self.users[1])
+#         match2.set_winner(self.users[3])
+#         next_round_match = TournamentMatch.objects.get(tournament=self.tournament_4, round=2, group=1)
+#         self.assertEqual(next_round_match.player1, self.users[1])
+#         self.assertEqual(next_round_match.player2, self.users[3])
+
+
+""" 
+before Round 1 - 1st match of Group 1: user3 vs user2, Winner: None Round 1 - 2nd match of Group 1: user4 vs user1, Winner: None
+after Round 1 - 1st match of Group 1: user3 vs user2, Winner: user3 Round 1 - 2nd match of Group 1: user4 vs user1, Winner: user1
+
+<QuerySet [
+<TournamentMatch: Round 1 - 1st match of Group 1: user3 vs user2, Winner: user3>,
+<TournamentMatch: Round 1 - 2nd match of Group 1: user4 vs user1, Winner: user1>
+<TournamentMatch: Round 2 - 1st match of Group 1: None vs None, Winner: None>,
+]>
+"""
